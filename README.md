685cde79f74d4844957f6dc1484d2ca26dc493a1
Gerald C. Manatad <gerald.manatad@evsu.edu.ph>
2025-05-08 23:29:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# CLI Nuber Guessing Game
+
+
+A simple command-line number guessing game where the computer randomly selects a number and the user has to guess it. 
+The user will be given a limited number of chances to guess the number. If the user guesses the number correctly, 
+the game will end, and the user will win. Otherwise, the game will continue until the user runs out of chances.
+
+## Features
+
+- When the game starts, it should display a welcome message along with the rules of the game.
+- The computer should randomly select a number between 1 and 100.
+- User should select the difficulty level (easy, medium, hard) which will determine the number of chances they get to guess the number.
+- The user should be able to enter their guess.
+- If the user’s guess is correct, the game should display a congratulatory message along with the number of attempts it took to guess the number.
+- If the user’s guess is incorrect, the game should display a message indicating whether the number is greater or less than the user’s guess.
+- The game should end when the user guesses the correct number or runs out of chances.
+
+## Installation
+
+1. Make sure you have [Node.js](https://nodejs.org) installed.
+
+2. Clone or download this project.
+
+3. To run use this command : "node cli_number_guessing_game.js"
+  - Usage: node cli_task_tracker.js
+  - Prompts:
+      -  1       Easy(10)
+      -  2       Medium(5)
+      -  3       Hard(3)
+      -  4       Show this help message
+      -  5       Quit
+
+4. https://roadmap.sh/projects/number-guessing-game
---
+++ cli_number_guessing_game.js
@@ -0,0 +1,120 @@
+#!/usr/bin/env node
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Parse command line arguments
+const args = process.argv.slice(2);
+const command = args[0];
+
+function printUsage() {
+  console.log('\nUsage: node cli_task_tracker.js');
+  console.log('Prompts:');
+  console.log('  1       Easy(10)');
+  console.log('  2       Medium(5)');
+  console.log('  3       Hard(3)');
+  console.log('  4       Show this help message');
+  console.log('  5       Quit');
+}
+
+function start_game(chances) {
+    const numberToGuess = Math.floor(Math.random() * 100) + 1;
+    const startTime = new Date(); // Start timer
+    let attempts = 0;
+
+
+    console.log(`I'm thinking of a number between 1 and 100.`);
+    console.log(`You have ${chances} chances to guess the correct number.\n`);
+
+    const guessNumber = () => {
+        if (attempts < chances) {
+            rl.question('\nEnter your guess: ', (input) => {
+                const guess = parseInt(input);
+                attempts++;
+                if (isNaN(guess) || guess < 1 || guess > 100) {
+                    console.log('Please enter a valid number between 1 and 100.');
+                    --attempts;
+                    guessNumber(); // Ask again
+                } else if (guess < numberToGuess) {
+                    console.log(`Incorrect! The number is greater than ${guess}.`);
+                    guessNumber(); // Ask again
+                } else if (guess > numberToGuess) {
+                    console.log(`Incorrect! The number is less than ${guess}.`);
+                    guessNumber(); // Ask again
+                } else {
+                    const endTime = new Date(); // End timer
+                    const timeDiff = Math.floor((endTime - startTime) / 1000); // Time in seconds
+                    console.log(`Congratulations! You guessed the correct number in ${attempts} attempts.`);
+                    console.log(`It took you ${timeDiff} seconds.`);
+                   // rl.close();
+                    selectDifficulty(); // Ask again
+                }
+            });
+        } else {
+            const endTime = new Date(); // End timer in case user fails
+            const timeDiff = Math.floor((endTime - startTime) / 1000); // Time in seconds
+            console.log(`\nSorry! You've used all your chances. The correct number was ${numberToGuess}.`);
+            console.log(`You took ${timeDiff} seconds.`);
+           // rl.close();
+            selectDifficulty(); // Ask again
+        }
+    };
+
+    guessNumber();
+}
+
+const selectDifficulty = () => {
+    console.log('\nWelcome to the Number Guessing Game!');
+    console.log('Please select the difficulty level:');
+    console.log('\n1. Easy (10 chances)');
+    console.log('2. Medium (5 chances)');
+    console.log('3. Hard (3 chances)');
+    console.log('4. Help');
+    console.log('5. Quit');
+
+    rl.question('\nEnter your choice: ', (choice) => {
+        switch (choice) {
+            case '1': {
+                console.log('\nGreat! You have selected the Easy difficulty level.');
+                start_game(10);           
+                break;
+            }
+        
+            case '2': {
+                console.log('\nGreat! You have selected the Medium difficulty level.');
+                start_game(5);
+                break;
+            }
+        
+            case '3': {
+                console.log('\nGreat! You have selected the Hard difficulty level.');
+                start_game(3);
+                break;
+            }
+
+            case '4': {
+                printUsage();
+                rl.close();
+                break;
+            }
+        
+            case '5': {
+                console.log('Game exited.');
+                rl.close();
+                break;
+            }
+        
+            default: {
+                console.log('Invalid choice. Please select a valid difficulty level.');
+                selectDifficulty(); // Ask again
+                break;
+            }
+        }
+    });
+}
+
+selectDifficulty();
+
